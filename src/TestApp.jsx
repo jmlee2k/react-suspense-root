3dfe6c70a94f0330259323dbdda748a2935f8399
jmlee2k <jmlee2k@gmail.com>
2022-08-12 10:34:35 -0400

    added invokeCount and clickCount

--- src/TestApp.jsx
+++ src/TestApp.jsx
@@ -1,10 +1,18 @@
-import { lazy, Suspense, useRef } from "react";
+import { lazy, Suspense, useRef, useState } from "react";
 const B = lazy(()=>import("./B"));
+
+let invokeCount = 0;
 
 export default function TestApp()
 {
-	const counter = useRef(0);
-	counter.current++;
-	console.log("rendering TestApp - counter",counter.current);
-	return <Suspense fallback={<p>loading B...</p>}><B/></Suspense>
+	const renderCount = useRef(0);
+	const [clickCount,setClickCount] = useState(0);
+	renderCount.current++;
+	invokeCount++;
+	console.log("rendering TestApp - render count",renderCount.current,"invoke count",invokeCount,"click count",clickCount);
+	return <>
+		<Suspense fallback={<p>loading B...</p>}><B/></Suspense>
+		<button onClick={()=>{console.log("clicked"); setClickCount(c=>c+1);}}>inc</button>
+		<p>click count: {clickCount}</p>
+	</>
 }
